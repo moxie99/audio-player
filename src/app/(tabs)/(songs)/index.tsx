e75c7dda4a044e6d4d880dbc8bc6361807145f59
Adeolu David <adeolusegun99@gmail.com>
2024-05-31 12:31:32 +0100

    bootsrapped projects and stated the colors to be used

---
+++ src/app/(tabs)/(songs)/index.tsx
@@ -0,0 +1,43 @@
+import { TracksList } from '@/components/TracksList'
+import { screenPadding } from '@/constants/tokens'
+import { trackTitleFilter } from '@/helpers/filter'
+import { generateTracksListId } from '@/helpers/miscellaneous'
+import { useNavigationSearch } from '@/hooks/useNavigationSearch'
+import { useTracks } from '@/store/library'
+import { defaultStyles } from '@/styles'
+import { useMemo } from 'react'
+import { ScrollView } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+const SongsScreen = () => {
+	const search = useNavigationSearch({
+		searchBarOptions: {
+			placeholder: 'Find in songs',
+		},
+	})
+
+	const tracks = useTracks()
+
+	const filteredTracks = useMemo(() => {
+		if (!search) return tracks
+
+		return tracks.filter(trackTitleFilter(search))
+	}, [search, tracks])
+
+	return (
+		<SafeAreaView style={defaultStyles.container}>
+			<ScrollView
+				contentInsetAdjustmentBehavior="automatic"
+				style={{ paddingHorizontal: screenPadding.horizontal }}
+			>
+				<TracksList
+					id={generateTracksListId('songs', search)}
+					tracks={filteredTracks}
+					scrollEnabled={false}
+				/>
+			</ScrollView>
+		</SafeAreaView>
+	)
+}
+
+export default SongsScreen
